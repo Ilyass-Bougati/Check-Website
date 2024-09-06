a97c50368d6abce83f35c4f3e611aef37e634afe
Ilyass-Bougati <110248648+Ilyass-Bougati@users.noreply.github.com>
2024-09-06 18:37:31 +0100

    Added a gui

---
+++ app.py
@@ -0,0 +1,60 @@
+import wx
+from check import Check, RUNNING
+import threading
+import sys
+
+
+class MyFrame(wx.Frame):    
+    def __init__(self):
+        super().__init__(parent=None, title='Checker')
+        panel = wx.Panel(self)
+
+        self.my_sizer = wx.BoxSizer(wx.VERTICAL)
+        self.url_input = wx.TextCtrl(panel)
+        self.url_label = wx.StaticText(panel, label="URL")
+        self.my_sizer.Add(self.url_label, 0, wx.ALL | wx.EXPAND, 5)
+        self.my_sizer.Add(self.url_input, 0, wx.ALL | wx.EXPAND, 5)
+
+        self.tag_input = wx.TextCtrl(panel)
+        self.tag_label = wx.StaticText(panel, label="TAG")
+        self.my_sizer.Add(self.tag_label, 0, wx.ALL | wx.EXPAND, 5)
+        self.my_sizer.Add(self.tag_input, 0, wx.ALL | wx.EXPAND, 5)  
+
+        my_btn = wx.Button(panel, label='Check')
+        my_btn.Bind(wx.EVT_BUTTON, self.OnClicked)
+        self.my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 5)        
+        panel.SetSizer(self.my_sizer)      
+
+        self.logs = wx.StaticText(panel, label="")
+        self.my_sizer.Add(self.logs, 0, wx.ALL | wx.EXPAND, 5)
+
+        self.Show()
+        self.Bind(wx.EVT_CLOSE, self.OnClose)
+
+    def OnClicked(self, event): 
+        if self.url_input.Value == "" or self.tag_input.Value == "":
+            self.Error("Fill the inputs", "Input error")
+            return 
+        args = (self.url_input.Value, self.tag_input.Value, self)
+        self.t = threading.Thread(target=Check, args=args)
+        self.t.start()
+
+    def Print(self, msg: str):
+        self.logs.Label = msg
+        
+    def Error(self, msg: str, title: str):
+        wx.MessageBox(msg, title, wx.OK | wx.ICON_ERROR)
+
+    def OnClose(self, event):
+        self.t._get_my_tid()
+        self.Destroy()
+
+
+
+
+
+if __name__ == '__main__':
+    app = wx.App()
+    frame = MyFrame()
+    app.MainLoop()
+    RUNNING = False
--- check.py
+++ check.py
@@ -2,64 +2,61 @@ import requests
 from time import sleep
 from notifypy import Notify
 from bs4 import BeautifulSoup
-from sys import argv
 
+RUNNING = True
 WAIT_TIME = 60
 
-# Checking if the url exists
-if len(argv) != 3:
-    url = input("url : ")
-    tag = input("tag : ")
-else:
-    url = url = argv[1]
-    tag = argv[2]
+def Check(url: str, tag: str, frame):
+    url = url
+    tag = str
 
+    # Create a notification object
+    notification = Notify()
+    # getting the initial page
 
-# Create a notification object
-notification = Notify()
-# getting the initial page
+    print(f"Getting the provided url...")
+    try:
+        initial_request = requests.get(url)
+    except:
+        frame.Error("Url is unreachable. You may be blocked, the url isn't correct, the website is down or my code is shit ;)", "Network error")
 
-print(f"Getting the provided url...")
-try:
-    initial_request = requests.get(url)
-except:
-    print("Error getting the url")
-    exit(1)
-
-    
-# checking if the url is reachable
-if initial_request.status_code != 200:
-    print("Error getting the url")
-    print(f"http status code = {initial_request.status_code}")
-
-print("Website reached successfully")
-print("page saved to memory, you'll be notified for any change")
-init_list = BeautifulSoup(initial_request.text, features="html.parser").find_all(tag)
-
-# checking if the page changed
-i = 1
-while True:
-    # waiting 
-    sleep(WAIT_TIME)
-
-    # getting the page
-    req = requests.get(url)
-    if req.status_code != 200:
-        print("Error getting the url")
-        print(f"http status code = {req.status_code}")
-    new_list = BeautifulSoup(initial_request.text, features="html.parser").find_all(tag)
-
-    if init_list != new_list:
-        # Set the title and message for the notification
-        notification.title = "Spotted a change in the web page"
-        notification.message = f"Something changed in\n{url}"
-        notification.urgency = "critical"
-        notification.icon = "icon/icon.png"
-        # Display the notification
-        notification.send()
         
-        init_list = new_list
-    else :
-        print(f"Check {i}: Nothing changed", end="\r")
+    # checking if the url is reachable
+    if initial_request.status_code != 200:
+        frame.Error(f"http status code = {initial_request.status_code}", "Error getting the url")
 
-    i = i + 1
+    frame.Print("Website reached successfully")
+    frame.Print("page saved to memory, you'll be notified for any change")
+    init_list = BeautifulSoup(initial_request.text, features="html.parser").find_all(tag)
+
+    i = 1
+    # checking if the page changed
+    while RUNNING:
+        # waiting 
+        sleep(WAIT_TIME)
+
+        # getting the page
+        req = requests.get(url)
+        if req.status_code != 200:
+            frame.Error(f"http status code = {req.status_code}", "Error getting the url")
+
+        try:
+            new_list = BeautifulSoup(initial_request.text, features="html.parser").find_all(tag)
+        except:
+            frame.Error("Error parsing or searching through the page", "Parsing error")
+
+
+        if init_list != new_list:
+            # Set the title and message for the notification
+            notification.title = "Spotted a change in the web page"
+            notification.message = f"Something changed in\n{url}"
+            notification.urgency = "critical"
+            notification.icon = "icon/icon.png"
+            # Display the notification
+            notification.send()
+            
+            init_list = new_list
+        else:
+            frame.Print(f"Attempt {i}")
+        i = i + 1
+
